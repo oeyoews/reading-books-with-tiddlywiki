17f7bb469276fbdb5263119e3fd4789ab2c0b99e
oeyoews <jyao4783@gmail.com>
2023-10-25 19:57:47 +0800

    update

--- src/generatepluginbook.ts
+++ src/generatepluginbook.ts
@@ -165,7 +165,7 @@ export const generateBookInfo = (bookinfo) => {
 
 > ''书籍名'': ${bookname || "未知"}\n
 > ''书籍作者'': ${author || "未知"}\n
-> ''书籍大小'': {{!!size}} kb\n
+> ''书籍大小'': ${kb} kb\n
 > ''插件最后构建时间'': {{!!updatetime}} \n
 > ''简要描述'': ${description || "未知"}
 >  Maked By [[reading books with tiddlywiki|https://github.com/oeyoews/reading-books-with-tiddlywiki]]
