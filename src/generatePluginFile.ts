35a9c15b390bc578694071fbaff9cc728a86491c
oeyoews <jyao4783@gmail.com>
2023-10-26 11:03:41 +0800

    split files

---
+++ src/generatePluginFile.ts
@@ -0,0 +1,102 @@
+import fs from "fs";
+import path from "path";
+import { getFolderSize } from "@/getFolderSize";
+import chalk from "chalk";
+import rimraf from "rimraf";
+
+export function generatePluginFile(toc, bookinfo) {
+  const defaultcover = `//github.com/oeyoews/reading-books-with-tiddlywiki/blob/main/image/${bookinfo.bookname}.png?raw=true`;
+  const {
+    bookname,
+    author,
+    description,
+    cover = defaultcover,
+    version = "0.0.1",
+  }: BookInfo = bookinfo;
+  const pluginPrefix = "$:/plugins/books";
+  const outputDir = "plugins";
+  const plugindir = path.join(outputDir, bookname);
+  const pluginfiledir = path.join(plugindir, "files");
+
+  const tocContent = toc
+    .map(
+      ({ currentLink, vanillatitle }) => `# [[${vanillatitle}|${currentLink}]]`
+    )
+    .join("\n");
+
+  fs.writeFileSync(path.join(pluginfiledir, `${bookname}目录.tid`), tocContent);
+
+  const { kb, mb } = getFolderSize(path.join(outputDir, bookname));
+  // 生成 TiddlyWiki 文件和目录结构
+  const tiddlywikifiles = {
+    tiddlers: [
+      {
+        file: `${bookname}目录.tid`,
+        fields: {
+          title: {
+            source: "basename",
+          },
+          // type: "text/vnd.tiddlywiki",
+          tags: ["toc", bookname],
+          caption: bookname,
+        },
+      },
+    ],
+    directories: [
+      {
+        path: ".",
+        filesRegExp: "^.*\\.tid$",
+        isTiddlerFile: false,
+        fields: {
+          title: {
+            source: "basename",
+          },
+          // type: "text/vnd.tiddlywiki",
+          tags: `${bookname}`,
+        },
+      },
+    ],
+  };
+
+  const readmecontent = `title: ${pluginPrefix}/${bookname}/readme
+
+<img src='${cover}' alt='' class="spotlight ${bookname}"/>
+
+> ''书籍'': ${bookname || "未知"}\n
+> ''作者'': ${author || "未知"}\n
+> ''大小'': ${kb} kb\n
+> ''构建时间'': {{!!updatetime}} \n
+> ''简要描述'': ${description || "未知"}
+>  Maked By [[reading books with tiddlywiki|https://github.com/oeyoews/reading-books-with-tiddlywiki]]
+
+> <button>[[开始阅读 |${bookname}目录]]</button>
+
+`;
+
+  const plugininfo = {
+    updatetime: new Date().toLocaleString(),
+    size: kb,
+    title: `${pluginPrefix}/${bookname}`,
+    author: "oeyoews",
+    "book#author": author,
+    description: bookname,
+    cover,
+    caption: bookname,
+    book: bookname,
+    type: "plugin",
+    version,
+    list: `readme`,
+  };
+
+  fs.writeFileSync(path.join(outputDir, bookname, "readme.tid"), readmecontent);
+  fs.writeFileSync(
+    path.join(outputDir, bookname, "plugin.info"),
+    JSON.stringify(plugininfo, null, 2)
+  );
+
+  fs.writeFileSync(
+    `${pluginfiledir}/tiddlywiki.files`,
+    JSON.stringify(tiddlywikifiles, null, 2)
+  );
+  console.log(chalk.green.bold(`《《${bookname}》》 书籍制作完成. ${mb} Mb`));
+}
--- src/generatepluginbook.ts
+++ src/generatepluginbook.ts
@@ -2,11 +2,11 @@ import fs from "fs";
 import cheerio, { CheerioAPI, Element, Cheerio } from "cheerio";
 import path from "path";
 import MarkdownIt from "markdown-it";
-import { rimraf } from "rimraf";
 import chalk from "chalk";
-import { getFolderSize } from "@/getFolderSize";
 import { getTitle } from "@/getTitle";
 import { generateTOC } from "@/generateTOC";
+import { generatePluginFile } from "@/generatePluginFile";
+import { rimraf } from "rimraf";
 
 const md = new MarkdownIt({
   linkify: true,
@@ -17,25 +17,17 @@ const md = new MarkdownIt({
 export const generateBookInfo = (bookinfo: BookInfo) => {
   // TODO: 默认将图片打包到插件
   // NOTE: github 禁止跨域， 需要移除https, 动态检测
-  const pluginPrefix = "$:/plugins/books";
-  const defaultcover = `//github.com/oeyoews/reading-books-with-tiddlywiki/blob/main/image/${bookinfo.bookname}.png?raw=true`;
-  const {
-    bookname,
-    author,
-    description,
-    cover = defaultcover,
-    version = "0.0.1",
-    disable = false,
-  }: BookInfo = bookinfo;
+  const { bookname, disable = false }: BookInfo = bookinfo;
   if (disable) {
     console.log(chalk.red.bold(`《《${bookname}》》 在黑名单中， 跳过制作`));
     return;
   }
   const outputDir = "plugins";
-  const bookOutputDir = path.join(outputDir, bookname, "files");
+  const plugindir = path.join(outputDir, bookname);
+  const pluginfiledir = path.join(plugindir, "files");
 
-  rimraf.moveRemoveSync(path.join(outputDir, bookname));
-  fs.mkdirSync(bookOutputDir, { recursive: true });
+  rimraf.moveRemoveSync(plugindir);
+  fs.mkdirSync(pluginfiledir, { recursive: true });
 
   const markdown = fs.readFileSync(
     path.join("markdown", `${bookname}.md`),
@@ -74,10 +66,12 @@ export const generateBookInfo = (bookinfo: BookInfo) => {
   });
 
   headings.each((index, heading) => {
-    processHeading(heading, index, padLength);
+    processHeading(heading, index);
   });
 
-  function processHeading(heading: Element, index: number, padLength = 3) {
+  generatePluginFile(toc, bookinfo);
+
+  function processHeading(heading: Element, index: number) {
     const headingContent = $(heading);
     // /\s{2,}/g
     const vanillatitle = headingContent.text().replace("s+/g", " ");
@@ -108,96 +102,13 @@ export const generateBookInfo = (bookinfo: BookInfo) => {
     const content = `${headingAllContent}\n\n${prevChapterLink}${nextChapterLink}`;
 
     try {
-      const filename = path.join(bookOutputDir, `${currentLink}.tid`);
+      const filename = path.join(pluginfiledir, `${currentLink}.tid`);
       fs.writeFileSync(filename, content);
     } catch (error) {
       console.error(`Failed to save file: ${error.message}`);
       return;
     }
   }
-
-  // 生成目录文件
-  const tocContent = toc
-    .map(
-      ({ currentLink, vanillatitle }) => `# [[${vanillatitle}|${currentLink}]]`
-    )
-    .join("\n");
-
-  fs.writeFileSync(path.join(bookOutputDir, `${bookname}目录.tid`), tocContent);
-
-  const { kb, mb } = getFolderSize(path.join(outputDir, bookname));
-  // 生成 TiddlyWiki 文件和目录结构
-  const tiddlywikifiles = {
-    tiddlers: [
-      {
-        file: `${bookname}目录.tid`,
-        fields: {
-          title: {
-            source: "basename",
-          },
-          // type: "text/vnd.tiddlywiki",
-          tags: ["toc", bookname],
-          caption: bookname,
-        },
-      },
-    ],
-    directories: [
-      {
-        path: ".",
-        filesRegExp: "^.*\\.tid$",
-        isTiddlerFile: false,
-        fields: {
-          title: {
-            source: "basename",
-          },
-          // type: "text/vnd.tiddlywiki",
-          tags: `${bookname}`,
-        },
-      },
-    ],
-  };
-
-  const readmecontent = `title: ${pluginPrefix}/${bookname}/readme
-
-<img src='${cover}' alt='' class="spotlight ${bookname}"/>
-
-> ''书籍'': ${bookname || "未知"}\n
-> ''作者'': ${author || "未知"}\n
-> ''大小'': ${kb} kb\n
-> ''构建时间'': {{!!updatetime}} \n
-> ''简要描述'': ${description || "未知"}
->  Maked By [[reading books with tiddlywiki|https://github.com/oeyoews/reading-books-with-tiddlywiki]]
-
-> <button>[[开始阅读 |${bookname}目录]]</button>
-
-`;
-
-  const plugininfo = {
-    updatetime: new Date().toLocaleString(),
-    size: kb,
-    title: `${pluginPrefix}/${bookname}`,
-    author: "oeyoews",
-    "book#author": author,
-    description: bookname,
-    cover,
-    caption: bookname,
-    book: bookname,
-    type: "plugin",
-    version,
-    list: `readme`,
-  };
-
-  fs.writeFileSync(path.join(outputDir, bookname, "readme.tid"), readmecontent);
-  fs.writeFileSync(
-    path.join(outputDir, bookname, "plugin.info"),
-    JSON.stringify(plugininfo, null, 2)
-  );
-
-  fs.writeFileSync(
-    `${bookOutputDir}/tiddlywiki.files`,
-    JSON.stringify(tiddlywikifiles, null, 2)
-  );
-  console.log(chalk.green.bold(`《《${bookname}》》 书籍制作完成. ${mb} Mb`));
 
   if (!fs.existsSync("HTML")) fs.mkdirSync("HTML");
   fs.writeFileSync(path.join("HTML", `${bookname}.html`), md2html);
